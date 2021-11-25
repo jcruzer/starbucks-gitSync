48af68ccff51423475ea97792bf95520d09dae62
jcruzer <jcruzer@naver.com>
2021-11-25 17:45:22 +0900

    21.11.25 p1-ch12

--- js/main.js
+++ js/main.js
@@ -22,3 +22,29 @@ searchInputEl.addEventListener('blur', function(){
   searchInputEl.setAttribute('placeholder', '');
   // [setAttribute]html 속성 추가하는 함수
 });
+
+const badgeEl = document.querySelector('header .badges');
+
+// [lodash - throttle]~초단위로 부하를 줘서 함수가 실행되게 만드는 함수 스크롤작업할때 많이 쓰는 함수
+// ._throttle(함수, 시간ms)
+window.addEventListener('scroll', _.throttle(function(){
+  console.log(window.scrollY);
+  if(window.scrollY > 500){
+    // 뱃지 숨기기
+    // badgeEl.style.display = 'none';
+    // gsap.to(요소,지속시간,옵션); 옵션은 보통 객체데이터로 {여러개 씀 여기서 숫자는 그대로, 문자는 '' 사용}
+    gsap.to(badgeEl, .6, {
+      opacity:0,
+      display:'none'
+    });
+    
+  }
+  else{
+    // 뱃지 보이기
+    // badgeEl.style.display = 'block';
+    gsap.to(badgeEl, .6, {
+      opacity:1,
+      display:'block'
+    });
+  }
+}, 300));
